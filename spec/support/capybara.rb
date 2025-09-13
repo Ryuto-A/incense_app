bff058ef923a46b42d819a867053254dc495317e
Ryuto-A <anzai.ryuto0405@gmail.com>
2025-09-13 11:53:20 +0900

    0913 CI(GitHub Actions)の実装

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -12,3 +12,21 @@ RSpec.configure do |config|
     driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
   end
 end
+
+# CI環境用のドライバ分岐
+if ENV["CI"]
+  Capybara.register_driver :selenium_headless_ci do |app|
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1400,1400")
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  end
+
+  RSpec.configure do |config|
+    config.before(:each, type: :system, js: true) do
+      driven_by :selenium_headless_ci
+    end
+  end
+end
