029397d74200b55d01cb78b5c4c58782e7eada73
Ryuto-A <anzai.ryuto0405@gmail.com>
2026-03-11 19:24:19 +0900

    0311 test環境の設定変更

--- spec/system/pages_contact_spec.rb
+++ spec/system/pages_contact_spec.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
-
 require "rails_helper"
 
 RSpec.describe "Contact page", type: :system do
   it "is accessible without login" do
     visit contact_path
     expect(page).to have_content("お問い合わせ")
-    expect(page).to have_link("お問い合わせフォームへ")
+
+    if Rails.configuration.x.contact_form_url.present?
+      expect(page).to have_link("お問い合わせフォームへ")
+    else
+      expect(page).to have_content("未設定")
+    end
   end
 end
